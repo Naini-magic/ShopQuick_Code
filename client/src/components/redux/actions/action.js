caf63e705c407afddcf38adbbf66134d9b176b52
Sunaini Raghuwanshi <144336156+Naini-magic@users.noreply.github.com>
2025-05-04 10:55:37 +0530

    Update action.js url

--- client/src/components/redux/actions/action.js
+++ client/src/components/redux/actions/action.js
@@ -2,7 +2,7 @@ export const getProducts = () => async(dispatch) => {
   // we can also use axcios instead of this
   try{
     //  const data = await fetch("/getproducts" , {
-     const data = await fetch("https://shopquick-backend.onrender.com/getproducts" , {
+     const data = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getproducts` , {
         method: "GET",
         headers: {
             "Content-Type" : "application/json"
